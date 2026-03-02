6bbb20961a599520b2ae84e959cc5aa89755fcf4
AbrarRajin <abrarrajin@gmail.com>
2026-03-02 22:29:02 +0600

    Add Gmail, Searchbar, and Quicklinks modules; update manifest and newtab structure

--- newtab.js
+++ newtab.js
@@ -3,9 +3,16 @@
 import { initWeather } from "./modules/weather/weather.js";
 import { initFootball } from "./modules/football/football.js";
 import { initBackground } from "./modules/background/background.js";
-
-
+import { initGmail } from "./modules/gmail/gmail.js";
+import { initSearchbar } from "./modules/searchbar/searchbar.js";
+import { initQuicklinks } from "./modules/quicklinks/quicklinks.js";
 
 initWeather(document.getElementById("module-weather"));
 initFootball(document.getElementById("module-football"));
 initBackground(document.getElementById("module-background"));
+initGmail(document.getElementById("module-gmail"));
+initSearchbar(document.getElementById("module-searchbar"));
+initQuicklinks(document.getElementById("module-quicklinks"));
+
+
+
